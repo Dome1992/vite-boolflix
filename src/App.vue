e2942c48cc8976442bb64e76beeec1add2a1d991
Dome1992 <forlanodomenico92@mail.com>
2024-01-11 15:14:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import { store } from './store.js';
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+
+<template>
+ <h1>La mia nuova App </h1>
+</template>
+
+<style lang="scss"scoped>
+
+</style>
